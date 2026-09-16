00c1b8abb2c128c81eee530b347f54873ceb99f4
Victor Hernandez <victor160700@gmail.com>
2026-09-16 15:45:09 -0500

    feat: integrar busqueda desde navbar

--- E_frontend/src/app/layout/navbar/navbar.ts
+++ E_frontend/src/app/layout/navbar/navbar.ts
@@ -1,23 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router, RouterLink} from '@angular/router';
 
 import { CartService } from '../../shared/services/cart.service';
 import { AuthService } from '../../shared/services/auth.service';
 import { FavoriteService } from '../../shared/services/favorite.service';
+import { ProductService } from '../../shared/services/product.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  searchTerm = '';
+
   constructor(
     public cartService: CartService,
     public authService: AuthService,
     public favoriteService: FavoriteService,
+    public productService: ProductService,
     private router: Router
   ) {}
+
+  search(): void {
+    const term = this.searchTerm.trim();
+
+    this.productService.setSearchTerm(term);
+    this.router.navigate(['/catalog']);
+  }
 
   logout(): void {
     this.authService.logout()
